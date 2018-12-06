601dc80544799778ea7209aa0533692ab0a20643
tayloran <tayloran@rose-hulman.edu>
2018-12-06 11:38:48 -0500

    made both motors run for 6 seconds

--- src/m1_motion.py
+++ src/m1_motion.py
@@ -2,8 +2,8 @@
 An opportunity to explore how to make an EV3 Robot move.
 
 Authors: Dave Fisher, David Mutchler, Vibha Alangar,
-their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+their colleagues, and Alyssa Taylor.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # -----------------------------------------------------------------------------
 # TODO: 2.
@@ -32,8 +32,13 @@ def go_two_seconds():
     # -------------------------------------------------------------------------
     left_motor = rb.LargeMotor(rb.Plug("B"))  # Constructs Motor for left wheel
     left_motor.start()
-    time.sleep(2)
+    time.sleep(6)
     left_motor.brake()
+
+    right_motor = rb.LargeMotor(rb.Plug("C"))  # Constructs Motor for right wheel
+    right_motor.start()
+    time.sleep(6)
+    right_motor.brake()
 
 
 def run_test_go_inches():
